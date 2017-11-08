941b47ae13159ed64604b494482dbe6f8fbfe692
Reuben Aronson <rmaronson@cmu.edu>
2017-11-07 19:12:06 -0500

    update includes for opencv2

--- src/apriltags.cpp
+++ src/apriltags.cpp
@@ -33,8 +33,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <sensor_msgs/Image.h>
 #include <image_transport/image_transport.h>
 #include <visualization_msgs/Marker.h>
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
+// #include <opencv2/cv.h>
+#include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
 #include <cv_bridge/cv_bridge.h>
 
 // #include <src/TagDetector.h>
